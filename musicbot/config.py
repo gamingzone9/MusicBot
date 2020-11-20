cad352ff3e9f09e9d8cadc03e3ef7269a663ab10
gamingzone9 <48872387+gamingzone9@users.noreply.github.com>
2020-11-20 20:02:14 +0530

    Update config.py

--- musicbot/config.py
+++ musicbot/config.py
@@ -134,8 +134,7 @@ class Config:
         if not self._login_token:
             raise HelpfulError(
                 "No bot token was specified in the config.",
-                "As of v1.9.6_1, you are required to use a Discord bot account. "
-                "See https://github.com/Just-Some-Bots/MusicBot/wiki/FAQ for info.",
+                "As of v1.9.6_1, you are required to use a Discord bot account. ",
                 preface=self._confpreface
             )
 
